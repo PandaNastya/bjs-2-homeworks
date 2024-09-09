15ca4aae651c857b560a70dd5443d6a3b3155790
Nastya Zenkovetc <lebed1412@yandex.ru>
2024-09-09 19:28:48 +0300

    Первое задание выполнено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  const d = (Math.pow(b, 2) - 4 * a * c);
+
+  if (d > 0) {
+    const root1 = (-b + Math.sqrt(d)) / (2 * a);
+    const root2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr = [root1, root2];
+  } else if (d === 0) {
+    const root = -b / (2 * a);
+    arr = [root];
+  } else {
+    arr = [];
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = percent / 100;
+  const loanbody = amount - contribution;
+  const p = percent / 12;
+  const payment = loanbody * (p + (p / (((1 + p)**countMonths) - 1)));
+  amount = payment * countMonths;
+  return parseFloat(amount.toFixed(2));
 }
